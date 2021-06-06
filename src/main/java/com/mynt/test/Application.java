8deb0a6e5d7aaa7f6cfa1c41ccef9da86695c2bc
Rafael Liban <rafaelliban@gmail.com>
2021-06-06 14:11:31 +0800

    fix configuration properties

--- src/main/java/com/mynt/test/Application.java
+++ src/main/java/com/mynt/test/Application.java
@@ -1,9 +1,14 @@
 package com.mynt.test;
 
+import com.mynt.test.infra.voucher.VoucherProperties;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(VoucherProperties.class)
+@EnableEncryptableProperties
 public class Application {
 
 	public static void main(String[] args) {
