21491a5a8d8ad1f6d247ff1683b783df14600909
Rafael Liban <rafaelliban@gmail.com>
2021-06-07 15:26:52 +0800

    add docker build

--- src/main/java/com/mynt/test/Application.java
+++ src/main/java/com/mynt/test/Application.java
@@ -8,7 +8,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 @SpringBootApplication
 @EnableConfigurationProperties(VoucherProperties.class)
-@EnableEncryptableProperties
 public class Application {
 
 	public static void main(String[] args) {
